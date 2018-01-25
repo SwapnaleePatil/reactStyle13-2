1ddb6262b1b23e5986514ae5e6a38ccd363e92b2
swapnaleepatil <monapatil194@gmail.com>
2018-01-25 18:28:29 +0530

    File Of React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img className="img"   src="http://under30ceo.com/wp-content/uploads/2013/12/o-NEW-YORK-CITY-WRITER-facebook.jpg" />
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
